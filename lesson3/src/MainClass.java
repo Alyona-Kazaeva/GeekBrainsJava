f8a7ddd9809f8de5db79b113019d57a9a8c37d3c
Alena.Kazaeva <alyona.kazaeva@gmail.com>
2020-04-16 00:48:16 +0300

    homework 3 task 2

--- lesson3/src/MainClass.java
+++ lesson3/src/MainClass.java
@@ -6,6 +6,10 @@ public class MainClass {
     public static void main(String[] args) {
 
         guessNumber();
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        guessWord(words);
     }
 
     /**
@@ -39,6 +43,55 @@ public class MainClass {
             guessNumber();
         } else {
             System.out.println("До свидания!");
+        }
+    }
+
+    /**
+     * 2 * Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+     * "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+     * "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+     * При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+     * сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь. Если слово не угадано,
+     * компьютер показывает буквы которые стоят на своих местах.
+     * apple – загаданное
+     * apricot - ответ игрока
+     * ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+     * Для сравнения двух слов посимвольно, можно пользоваться:
+     * String str = "apple";
+     * str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
+     * Играем до тех пор, пока игрок не отгадает слово
+     * Используем только маленькие буквы
+     * @param words, массив слов для угадывания
+     */
+    private static void guessWord(String[] words) {
+        int randomIndex = (int) (Math.random() * words.length);
+        String findWord = words[randomIndex];
+        System.out.println("Введите слово");
+
+        while (true) {
+            String userWord = scanner.nextLine();
+            String guessedCharacters = "";
+            char currentSymbol;
+            int minLengthWord = Math.min(findWord.length(), userWord.length());
+            for (int i = 0; i < minLengthWord; i++) {
+                currentSymbol = findWord.charAt(i);
+                if (currentSymbol == userWord.charAt(i)) {
+                    guessedCharacters = guessedCharacters.concat(String.valueOf(currentSymbol));
+                } else {
+                    guessedCharacters = guessedCharacters.concat("#");
+                }
+            }
+            if (findWord.equals(guessedCharacters)) {
+                System.out.println("Вы угадали!");
+                break;
+            } else {
+                int lengthVar = 15 - guessedCharacters.length();
+                for (int i = 0; i < lengthVar; i++) {
+                    guessedCharacters = guessedCharacters.concat("#");
+                }
+                System.out.println("Угаданные буквы " + guessedCharacters);
+                System.out.println("Вы не угадали слово, попробуйте еще раз");
+            }
         }
     }
 
