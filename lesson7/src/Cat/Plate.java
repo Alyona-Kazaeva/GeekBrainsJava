5976e4bfa70c6bfd0e0dfcea95c7b304e267629a
Alena.Kazaeva <alyona.kazaeva@gmail.com>
2020-04-29 22:42:52 +0300

    homework 7

---
+++ lesson7/src/Cat/Plate.java
@@ -0,0 +1,29 @@
+package Cat;
+
+public class Plate {
+    private int food;
+
+    Plate(int food) {
+        this.food = food;
+    }
+
+    private void info() {
+        System.out.println("Еды в тарелке : " + food);
+    }
+
+    boolean decreaseFood(int amount, String name) {
+        if (food - amount < 0) {
+            System.out.println(name + " не поел, еды недостаточно");
+            return  false;
+        } else {
+            food -= amount;
+            System.out.println(name + " наелся");
+            return true;
+        }
+    }
+
+    void addFood(int amount){
+        food+=amount;
+        info();
+    }
+}
